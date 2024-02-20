bc262080bfbd865b636319a5968d4548f1db41ae
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-20 14:51:02 -0600

    Prepare base config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/canvas-rpg/" : "/",
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/javascript-action-multiplayer/"
+      : "/",
   resolve: {
     alias: [
       {
