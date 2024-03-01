97cea8ad81ac8815b8758350e9d729cbb5cc4421
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-03-01 10:50:22 -0600

    Enable damage animation on player

--- src/actors/Players/Player.ts
+++ src/actors/Players/Player.ts
@@ -15,6 +15,8 @@ import {
   LEFT,
   SCALE_2x,
   TAG_ANY_PLAYER,
+  TAG_DAMAGES_PLAYER,
+  TAG_PLAYER_WEAPON,
   UP,
 } from "@/constants";
 import { DirectionQueue } from "@/classes/DirectionQueue";
@@ -72,6 +74,9 @@ export class Player extends Actor {
     this.skinId = skinId;
     this.isPainFlashing = false;
     this.addTag(TAG_ANY_PLAYER);
+    this.on("collisionstart", (event) => {
+      this.onCollisionStart(event);
+    });
   }
 
   onInitialize(engine: Engine): void {
@@ -79,6 +84,19 @@ export class Player extends Actor {
     this.playerAnimations = new PlayerAnimations(this);
     this.playerActions = new PlayerActions(this);
     this.networkUpdater = new NetworkUpdater(engine, EVENT_SEND_PLAYER_UPDATE);
+  }
+
+  onCollisionStart(event: any) {
+    // Take damage from other player's weapons
+    if (event.other?.hasTag(TAG_PLAYER_WEAPON) && event.other.owner !== this) {
+      this.takeDamage();
+      event.other.onDamagedSomething();
+    }
+
+    // Take damage from external things (Enemies, etc)
+    if (event.other?.hasTag(TAG_DAMAGES_PLAYER)) {
+      this.takeDamage();
+    }
   }
 
   // Concats enough information to send to other players
