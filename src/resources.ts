24618180db638e3aa911f8cfac187df312fd5a90
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-03-01 10:05:21 -0600

    Add static asset

--- src/resources.ts
+++ src/resources.ts
@@ -6,6 +6,7 @@ export const Images = {
   blueSheetImage: new ImageSource("sprites/character-blue-sheet.png"),
   graySheetImage: new ImageSource("sprites/character-gray-sheet.png"),
   yellowSheetImage: new ImageSource("sprites/character-yellow-sheet.png"),
+  monsterSheetImage: new ImageSource("sprites/monster-sheet.png"),
 
   // Maps
   indoorImage: new ImageSource("maps/indoor.png"),
